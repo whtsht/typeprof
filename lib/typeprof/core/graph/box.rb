c0e142edffa773d7cffc3269da4582dbabd1153f
whtsht <whiteshirt0079@gmail.com>
2026-02-15 18:35:31 +0900

    Fix false type mismatch diagnostic when method body ends with explicit return

--- lib/typeprof/core/graph/box.rb
+++ lib/typeprof/core/graph/box.rb
@@ -311,6 +311,7 @@ module TypeProf::Core
 
     def wrong_return_type(f_ret_show, changes)
       actual_ty = @a_ret.show
+      return if actual_ty == "bot"
       return if actual_ty == "untyped" # XXX: too ad-hoc?
       msg = "expected: #{ f_ret_show }; actual: #{ actual_ty }"
       case @node
---
+++ scenario/rbs/check-return-type6.rb
@@ -0,0 +1,14 @@
+## update
+class Foo
+  #: (String) -> bool
+  def a(a)
+    return false
+  end
+end
+
+## assert
+class Foo
+  def a: (String) -> bool
+end
+
+## diagnostics
